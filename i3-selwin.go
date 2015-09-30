f04edde0ce8fde55bb65bf9bbb7c8d7f587e2eb3
Jun Wu <quark@lihdd.net>
2015-09-30 21:12:49 +0800

    Initial commit

---
+++ i3-selwin.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/proxypoke/i3ipc"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+type Candidate struct {
+	Name string
+	Id   int32
+}
+
+func dfsTree(t *i3ipc.I3Node) (cands []*Candidate) {
+	if t.Geometry.Width > 0 && t.Geometry.Height > 0 && t.Window > 0 {
+		cands = append(cands, &Candidate{Name: t.Name, Id: t.Id})
+	}
+	for _, c := range t.Nodes {
+		cands = append(cands, dfsTree(&c)...)
+	}
+	return
+}
+
+func checkError(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func main() {
+	ipc, e := i3ipc.GetIPCSocket()
+	checkError(e)
+
+	n, e := ipc.GetTree()
+	checkError(e)
+
+	cands := dfsTree(&n)
+
+	var input string
+	for _, c := range cands {
+		input += c.Name + "\n"
+	}
+
+	cmd := exec.Command("dmenu", flag.Args()...)
+	cmd.Stdin = strings.NewReader(input)
+
+	out, e := cmd.Output()
+	checkError(e)
+
+	name := strings.TrimRight(string(out), "\n")
+
+	var cand *Candidate
+	for _, c := range cands {
+		if c.Name == name {
+			cand = c
+			break
+		}
+	}
+
+	if cand != nil {
+		msg := fmt.Sprint("[con_id=", cand.Id, "] focus")
+		ipc.Raw(i3ipc.I3Command, msg)
+	}
+}
